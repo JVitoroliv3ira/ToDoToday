e52434f59a09e7bdbaa0f0eee387ddeb8309d599
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-14 17:34:12 -0300

    feature: Adiciona DTO para representar a request de cadastrar um novo usuario

---
+++ api/src/main/java/api/dtos/requests/UserRegistrationRequestDTO.java
@@ -0,0 +1,20 @@
+package api.dtos.requests;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class UserRegistrationRequestDTO {
+    @NotNull(message = "O nome não pode ser nulo.")
+    @Size(min = 5, max = 70, message = "O nome deve ter entre 5 e 70 caracteres.")
+    private String name;
+
+    @NotNull(message = "O e-mail não pode ser nulo.")
+    @Size(min = 5, max = 80, message = "O e-mail deve ter entre 5 e 80 caracteres.")
+    @Email(message = "Formato de e-mail inválido.")
+    private String email;
+
+    @NotNull(message = "A senha não pode ser nula.")
+    @Size(min = 6, max = 120, message = "A senha deve ter entre 6 e 120 caracteres.")
+    private String password;
+}

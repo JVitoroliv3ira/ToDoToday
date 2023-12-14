6496940c9d272416fcd4d44f3c60a992e9e92040
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-14 15:26:48 -0300

    feature: Cria modelo de usuario da aplicacao

---
+++ api/src/main/java/api/models/User.java
@@ -0,0 +1,46 @@
+package api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(schema = "todo_today", name = "tb_users")
+@Entity
+public class User {
+
+    @Id
+    @SequenceGenerator(name = "sq_users", schema = "todo_today", sequenceName = "sq_users", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sq_users")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password);
+    }
+}

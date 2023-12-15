0028fbad136621034e8ba538f0bc4baa946b9548
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-15 13:20:03 -0300

    feature: Implementa metodo de validar se o usuario existe com base no email

--- api/src/main/java/api/services/UserService.java
+++ api/src/main/java/api/services/UserService.java
@@ -1,5 +1,6 @@
 package api.services;
 
+import api.exceptions.UnprocessableEntityException;
 import api.models.User;
 import api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,5 +14,11 @@ public class UserService {
     public User create(User entity) {
         entity.setId(null);
         return this.repository.save(entity);
+    }
+
+    public void validateEmailUniqueness(String email) {
+        if (Boolean.TRUE.equals(this.repository.existsByEmail(email))) {
+            throw new UnprocessableEntityException("Este e-mail já está sendo utilizado.");
+        }
     }
 }

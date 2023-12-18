09a49c03349f91f42d701e146e5ad79a9b572fa8
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-18 14:59:26 -0300

    feature: Implementa util responsavel por encode e decode de informacoes do sistema

---
+++ api/src/main/java/api/utils/EncoderUtil.java
@@ -0,0 +1,12 @@
+package api.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class EncoderUtil {
+    private static final PasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public static String encode(String value) {
+        return encoder.encode(value);
+    }
+}

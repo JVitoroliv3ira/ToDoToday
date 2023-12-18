980e0384b362287bcb0d049c5428e126c1017073
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-18 15:10:26 -0300

    feature: Adiciona log nos handlers de exceptions

--- api/src/main/java/api/settings/GlobalExceptionHandler.java
+++ api/src/main/java/api/settings/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package api.settings;
 
 import api.dtos.responses.Response;
 import api.exceptions.UnprocessableEntityException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +12,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.List;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UnprocessableEntityException.class)
     public ResponseEntity<Response<Object>> handleUnprocessableEntityException(UnprocessableEntityException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.UNPROCESSABLE_ENTITY)
                 .body(new Response<>(null, List.of(ex.getMessage())));
@@ -23,13 +26,14 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Response<Object>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
-        List<String> erros = ex.getBindingResult()
+        log.error(ex.getMessage());
+        List<String> errors = ex.getBindingResult()
                 .getFieldErrors()
                 .stream().map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .toList();
 
         return ResponseEntity
                 .status(HttpStatus.UNPROCESSABLE_ENTITY)
-                .body(new Response<>(null, erros));
+                .body(new Response<>(null, errors));
     }
 }

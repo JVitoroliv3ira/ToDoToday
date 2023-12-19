066c4aff2c6861484f605168245da3268ce25af0
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-19 15:40:26 -0300

    feature: Implementa endpoint de login da aplicacao

--- api/src/main/java/api/controllers/v1/AuthController.java
+++ api/src/main/java/api/controllers/v1/AuthController.java
@@ -3,22 +3,23 @@ package api.controllers.v1;
 import api.dtos.requests.UserAuthenticationRequestDTO;
 import api.dtos.requests.UserRegistrationRequestDTO;
 import api.dtos.responses.Response;
+import api.dtos.responses.UserAuthenticationResponseDTO;
 import api.models.User;
+import api.services.AuthenticationService;
 import api.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping(path = "/api/v1/auth")
 @RestController
 public class AuthController {
     private final UserService userService;
+    private final AuthenticationService authService;
 
     @PostMapping(path = "/register")
     public ResponseEntity<Response<User>> register(@RequestBody @Valid UserRegistrationRequestDTO request) {
@@ -30,7 +31,12 @@ public class AuthController {
     }
 
     @PostMapping(path = "/login")
-    public ResponseEntity<Response<User>> login(@RequestBody @Valid UserAuthenticationRequestDTO request) {
-        return null;
+    public ResponseEntity<Response<UserAuthenticationResponseDTO>> login(@RequestBody @Valid UserAuthenticationRequestDTO request) {
+        UserDetails details = this.userService.loadUserByUsername(request.getEmail());
+        this.authService.validateCredentials(request.getPassword(), details.getPassword());
+        String token = this.authService.generateToken(request.getEmail());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new Response<>(new UserAuthenticationResponseDTO(details.getUsername(), token), null));
     }
 }

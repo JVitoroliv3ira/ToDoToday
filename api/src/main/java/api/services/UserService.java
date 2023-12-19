f660f92f18e6ec6bfe452323a28cb8783c472187
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-19 11:47:58 -0300

    feature: Implementa a interface UserDetailsService

--- api/src/main/java/api/services/UserService.java
+++ api/src/main/java/api/services/UserService.java
@@ -1,14 +1,18 @@
 package api.services;
 
+import api.dtos.DetailsDTO;
+import api.exceptions.BadRequestException;
 import api.exceptions.UnprocessableEntityException;
 import api.models.User;
 import api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserRepository repository;
 
     public User create(User entity) {
@@ -20,5 +24,13 @@ public class UserService {
         if (Boolean.TRUE.equals(this.repository.existsByEmail(email))) {
             throw new UnprocessableEntityException("Este e-mail já está sendo utilizado.");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws BadRequestException {
+        User user = this.repository
+                .findByEmail(email)
+                .orElseThrow(() -> new BadRequestException("Usuário não encontrado na base de dados."));
+        return new DetailsDTO(user);
     }
 }

9a0b10748aba46a6f19d5239eab1ceff56118bdd
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-20 14:25:54 -0300

    feature: Implementa testes unitarios para a classe UserService

--- api/src/test/java/api/services/UserServiceTest.java
+++ api/src/test/java/api/services/UserServiceTest.java
@@ -25,6 +25,9 @@ class UserServiceTest {
     private final UserRepository userRepository;
     private UserService userService;
 
+    private static final String USER_NOT_FOUND_MESSAGE = "Usuário não encontrado na base de dados.";
+    private static final String EMAIL_ALREADY_IN_USE_MESSAGE = "Este e-mail já está sendo utilizado.";
+
     @Autowired
     public UserServiceTest(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -72,6 +75,73 @@ class UserServiceTest {
     }
 
     @Test
+    void read_should_call_find_by_id_method_of_user_repository() {
+        User payload = this.buildUserPayload(1L, "payload", "payload@payload.com", "payload_123");
+        when(this.userRepository.findById(payload.getId())).thenReturn(Optional.of(payload));
+        this.userService.read(payload.getId());
+        verify(this.userRepository, times(1)).findById(payload.getId());
+    }
+
+    @Test
+    void read_should_throw_an_exception_when_requested_user_does_not_exists() {
+        Long payload = 34L;
+        when(this.userRepository.findById(payload)).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> this.userService.read(payload))
+                .hasMessage(USER_NOT_FOUND_MESSAGE)
+                .isInstanceOf(BadRequestException.class);
+    }
+
+    @Test
+    void update_should_call_exists_by_id_method_of_user_repository() {
+        User payload = this.buildUserPayload(1L, "payload", "payload@payload.com", "payload_123");
+        when(this.userRepository.existsById(payload.getId())).thenReturn(Boolean.TRUE);
+        this.userService.update(payload);
+        verify(this.userRepository, times(1)).existsById(payload.getId());
+    }
+
+    @Test
+    void update_should_throw_an_exception_when_requested_user_does_not_exists() {
+        User payload = this.buildUserPayload(1L, "payload", "payload@payload.com", "payload_123");
+        when(this.userRepository.existsById(payload.getId())).thenReturn(Boolean.FALSE);
+        assertThatThrownBy(() -> this.userService.update(payload))
+                .hasMessage(USER_NOT_FOUND_MESSAGE)
+                .isInstanceOf(BadRequestException.class);
+    }
+
+    @Test
+    void update_should_call_save_method_of_user_repository() {
+        User payload = this.buildUserPayload(1L, "payload", "payload@payload.com", "payload_123");
+        when(this.userRepository.existsById(payload.getId())).thenReturn(Boolean.TRUE);
+        this.userService.update(payload);
+        verify(this.userRepository, times(1)).save(payload);
+    }
+
+    @Test
+    void delete_should_call_exists_by_id_method_of_user_repository() {
+        Long payload = 36L;
+        when(this.userRepository.existsById(payload)).thenReturn(Boolean.TRUE);
+        this.userService.delete(payload);
+        verify(this.userRepository, times(1)).existsById(payload);
+    }
+
+    @Test
+    void delete_should_throw_an_exception_when_requested_user_does_not_exists() {
+        Long payload = 36L;
+        when(this.userRepository.existsById(payload)).thenReturn(Boolean.FALSE);
+        assertThatThrownBy(() -> this.userService.delete(payload))
+                .hasMessage(USER_NOT_FOUND_MESSAGE)
+                .isInstanceOf(BadRequestException.class);
+    }
+
+    @Test
+    void delete_should_call_delete_by_id_method() {
+        Long payload = 36L;
+        when(this.userRepository.existsById(payload)).thenReturn(Boolean.TRUE);
+        this.userService.delete(payload);
+        verify(this.userRepository, times(1)).deleteById(payload);
+    }
+
+    @Test
     void validate_email_uniqueness_should_call_exists_by_email_method_of_user_repository() {
         String payload = "payload@payload.com";
         when(this.userRepository.existsByEmail(payload)).thenReturn(Boolean.FALSE);
@@ -84,7 +154,7 @@ class UserServiceTest {
         String payload = "payload@payload.com";
         when(this.userRepository.existsByEmail(payload)).thenReturn(Boolean.TRUE);
         assertThatThrownBy(() -> this.userService.validateEmailUniqueness(payload))
-                .hasMessage("Este e-mail já está sendo utilizado.")
+                .hasMessage(EMAIL_ALREADY_IN_USE_MESSAGE)
                 .isInstanceOf(BadRequestException.class);
     }
 
@@ -101,7 +171,7 @@ class UserServiceTest {
         String payload = "payload@payload.com";
         when(this.userRepository.findByEmail(payload)).thenReturn(Optional.empty());
         assertThatThrownBy(() -> this.userService.loadUserByUsername(payload))
-                .hasMessage("Usuário não encontrado na base de dados.")
+                .hasMessage(USER_NOT_FOUND_MESSAGE)
                 .isInstanceOf(BadRequestException.class);
     }
 

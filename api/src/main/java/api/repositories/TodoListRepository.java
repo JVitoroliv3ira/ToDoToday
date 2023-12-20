9446b8a6d5b2791cdc173907403871c8a9719ffb
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-20 18:43:07 -0300

    feature: Implementa metodo responsavel por verificar a existencia de um todo list com base no id e no dono da lista

--- api/src/main/java/api/repositories/TodoListRepository.java
+++ api/src/main/java/api/repositories/TodoListRepository.java
@@ -1,9 +1,11 @@
 package api.repositories;
 
 import api.models.TodoList;
+import api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoListRepository extends JpaRepository<TodoList, Long> {
+    Boolean existsByIdAndOwner(Long id, User owner);
 }

a1cdcf2c94cb090b632b65b3175333abf6f095d8
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-20 18:43:37 -0300

    feature: Implementa metodo responsavel por validar se um usuario pode realizar acoes com a lista de tarefas

--- api/src/main/java/api/services/TodoListService.java
+++ api/src/main/java/api/services/TodoListService.java
@@ -1,7 +1,9 @@
 package api.services;
 
 import api.contracts.ICrudService;
+import api.exceptions.BadRequestException;
 import api.models.TodoList;
+import api.models.User;
 import api.repositories.TodoListRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,5 +21,11 @@ public class TodoListService implements ICrudService<TodoList, Long, TodoListRep
     @Override
     public String getNotFoundMessage() {
         return "Lista de tarefas não encontrada na base de dados.";
+    }
+
+    public void validateTodoListOwnership(Long id, User user) {
+        if (Boolean.FALSE.equals(this.repository.existsByIdAndOwner(id, user))) {
+            throw new BadRequestException("Você não tem permissão para realizar esta ação.");
+        }
     }
 }

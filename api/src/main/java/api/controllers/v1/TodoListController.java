d00916743bdc7e31557144f5ceadd86c44359a25
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-20 00:57:52 -0300

    feature: Cria o controller TodoListController

---
+++ api/src/main/java/api/controllers/v1/TodoListController.java
@@ -0,0 +1,12 @@
+package api.controllers.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/v1/todo-list")
+@RestController
+public class TodoListController {
+    
+}

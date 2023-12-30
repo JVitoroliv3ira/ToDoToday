21439c066c897836f9441061afa764a2cd82b80e
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-30 16:02:11 -0300

    feature: Implementa metodo responsavel por buscar as listas usando paginacao

--- api/src/main/java/api/repositories/TodoListRepository.java
+++ api/src/main/java/api/repositories/TodoListRepository.java
@@ -2,10 +2,14 @@ package api.repositories;
 
 import api.models.TodoList;
 import api.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoListRepository extends JpaRepository<TodoList, Long> {
     Boolean existsByIdAndOwner(Long id, User owner);
+
+    Page<TodoList> findAllByOwner(Pageable pageable, User owner);
 }

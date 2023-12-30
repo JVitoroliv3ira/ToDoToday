83883e15dac108d1950d4fb5a9170e6602265671
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-30 16:02:30 -0300

    feature: Implementa metodo responsavel por buscar as listas usando paginacao, convertendo de TodoList para TodoListDetailDTO

--- api/src/main/java/api/services/TodoListService.java
+++ api/src/main/java/api/services/TodoListService.java
@@ -1,11 +1,15 @@
 package api.services;
 
 import api.contracts.ICrudService;
+import api.dtos.responses.TodoListDetailResponseDTO;
 import api.exceptions.BadRequestException;
 import api.models.TodoList;
 import api.models.User;
 import api.repositories.TodoListRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -27,5 +31,10 @@ public class TodoListService implements ICrudService<TodoList, Long, TodoListRep
         if (Boolean.FALSE.equals(this.repository.existsByIdAndOwner(id, user))) {
             throw new BadRequestException("Você não tem permissão para realizar esta ação.");
         }
+    }
+
+    public Page<TodoListDetailResponseDTO> findAllByOwner(Integer pageNumber, Integer size, User owner) {
+        Pageable pageable = PageRequest.of(pageNumber, size);
+        return this.repository.findAllByOwner(pageable, owner).map(TodoListDetailResponseDTO::new);
     }
 }

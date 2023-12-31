ab729b89a60946d79236552e304e0021655965f2
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-30 22:20:15 -0300

    feature: Cria repositorio para o modelo de Todo

---
+++ api/src/main/java/api/repositories/TodoRepository.java
@@ -0,0 +1,9 @@
+package api.repositories;
+
+import api.models.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}

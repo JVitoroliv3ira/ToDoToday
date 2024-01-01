2af2cc2e5ecf2f1c89ca4419c174e33258e6ad6a
João Oliveira <jvitoroliveira0711@gmail.com>
2023-12-31 22:37:18 -0300

    feature: Implementa endpoint de cadastros de novos Todo

--- api/src/main/java/api/controllers/v1/TodoController.java
+++ api/src/main/java/api/controllers/v1/TodoController.java
@@ -1,8 +1,19 @@
 package api.controllers.v1;
 
+import api.dtos.requests.TodoCreationRequestDTO;
+import api.dtos.responses.Response;
+import api.dtos.responses.TodoDetailResponseDTO;
+import api.models.Todo;
+import api.models.User;
 import api.services.AuthenticationService;
+import api.services.TodoListService;
 import api.services.TodoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/v1/todo")
 @RestController
 public class TodoController {
+    private final TodoListService todoListService;
     private final TodoService todoService;
     private final AuthenticationService authenticationService;
+
+    @PostMapping(path = "/create")
+    public ResponseEntity<Response<TodoDetailResponseDTO>> create(@RequestBody @Valid TodoCreationRequestDTO request) {
+        User authenticatedUser = this.authenticationService.getAuthenticatedUser();
+        this.todoListService.validateTodoListOwnership(request.getTodoListId(), authenticatedUser);
+        Todo createdTodo = this.todoService.create(request.convert());
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new Response<>(new TodoDetailResponseDTO(createdTodo), null));
+    }
 }

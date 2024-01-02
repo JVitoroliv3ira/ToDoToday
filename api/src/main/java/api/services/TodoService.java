bf804bd46b6393375278536b53892c6458c97292
João Oliveira <jvitoroliveira0711@gmail.com>
2024-01-02 20:36:16 -0300

    feature: Implementa metodo que busca os todos utilizando paginacao

--- api/src/main/java/api/services/TodoService.java
+++ api/src/main/java/api/services/TodoService.java
@@ -1,9 +1,13 @@
 package api.services;
 
 import api.contracts.ICrudService;
+import api.dtos.responses.TodoDetailResponseDTO;
 import api.models.Todo;
 import api.repositories.TodoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -19,5 +23,10 @@ public class TodoService implements ICrudService<Todo, Long, TodoRepository> {
     @Override
     public String getNotFoundMessage() {
         return "Tarefa não encontrada na base de dados.";
+    }
+
+    public Page<TodoDetailResponseDTO> findAllByTodoListId(Integer pageNumber, Integer size, Long todoListId) {
+        Pageable pageable = PageRequest.of(pageNumber, size);
+        return this.repository.findAllByTodoListId(pageable, todoListId).map(TodoDetailResponseDTO::new);
     }
 }
